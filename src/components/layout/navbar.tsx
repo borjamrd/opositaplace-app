d842604060a5d1370aa6556a5b2968f37671a8e0
Borja <borjamrd1@gmail.com>
2025-06-02 20:31:22 +0200

    style: navbar container full

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -58,7 +58,7 @@ export function Navbar() {
   if (loading) {
     return (
       <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container px-4 md:px-10 flex h-16 max-w-screen-2xl items-center justify-between">
+        <div className="px-4 md:px-10 flex h-16 items-center justify-between">
           <Link href="/" className="flex items-center space-x-2">
             <Logo />
           </Link>
@@ -70,7 +70,7 @@ export function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container px-4 md:px-10 flex h-16 max-w-screen-2xl items-center justify-between">
+      <div className="px-4 md:px-10 flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
           <Logo />
         </Link>
