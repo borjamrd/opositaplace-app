f8d218d01cd93f3300efe6e40163e12d0076f495
Borja <borjamrd1@gmail.com>
2025-07-04 14:47:49 +0200

    tests: global resizeobserver added to setup

--- src/lib/tests/setup.ts
+++ src/lib/tests/setup.ts
@@ -1,2 +1,9 @@
 // src/lib/tests/setup.ts
 import '@testing-library/jest-dom';
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
